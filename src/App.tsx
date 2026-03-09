f29a6e335b21be527577296fe7eeab607527970d
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-03-08 23:01:42 -0300

    feat: update App component with react95 theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-const App = () => {
-  return (
-    <div className="font-bold">Olá!</div>
-  )
-}
+import { MenuList, MenuListItem, Separator, styleReset } from 'react95';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-export default App
+import original from 'react95/dist/themes/original';
+
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body, input, select, textarea {
+    font-family: 'ms_sans_serif';
+  }
+`;
+
+const App = () => (
+  <div>
+    <GlobalStyles />
+    <ThemeProvider theme={original}>
+      <MenuList>
+        <MenuListItem>🎤 Sing</MenuListItem>
+        <MenuListItem>💃🏻 Dance</MenuListItem>
+        <Separator />
+        <MenuListItem disabled>😴 Sleep</MenuListItem>
+      </MenuList>
+    </ThemeProvider>
+  </div>
+);
+
+export default App;
