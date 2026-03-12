fb232508432d0bceca11160aca32e976892b998f
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-03-11 22:07:57 -0300

    feat: add global styles with Tailwind CSS and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import { MenuList, MenuListItem, Separator, styleReset } from 'react95';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
-import original from 'react95/dist/themes/original';
+import original from 'react95/dist/themes/original'
 
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
-
-const GlobalStyles = createGlobalStyle`
-  ${styleReset}
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif}') format('woff2');
-    font-weight: 400;
-    font-style: normal
-  }
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif_bold}') format('woff2');
-    font-weight: bold;
-    font-style: normal
-  }
-  body, input, select, textarea {
-    font-family: 'ms_sans_serif';
-  }
-`;
+import Page from './page/page';
 
 const App = () => (
   <div>
-    <GlobalStyles />
     <ThemeProvider theme={original}>
-      <MenuList>
-        <MenuListItem>🎤 Sing</MenuListItem>
-        <MenuListItem>💃🏻 Dance</MenuListItem>
-        <Separator />
-        <MenuListItem disabled>😴 Sleep</MenuListItem>
-      </MenuList>
+      <Page />
     </ThemeProvider>
   </div>
 );
