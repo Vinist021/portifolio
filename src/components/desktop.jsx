493776bed1e082c9be92e059d5f62495e666e447
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-03-18 22:06:24 -0300

    feat(desktop): add 'Sobre' item to TaskBar

--- src/components/desktop.jsx
+++ src/components/desktop.jsx
@@ -1,10 +1,14 @@
 import { List, TaskBar } from "@react95/core";
+import { Explorer100, Explorer101 } from "@react95/icons";
 
 
 export default function Desktop() {
     return (
         <TaskBar list={
             <List width="225px">
+                <List.Item icon={<Explorer101 variant="32x32_4"/>}>
+                    Sobre
+                </List.Item>
             </List>
         } />
     )
