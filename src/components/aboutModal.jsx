2586113cf98258731b3815c08e3c063796ff0bc0
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-04-01 22:26:11 -0300

    feat(sobre): implementa texto descritivo

--- src/components/aboutModal.jsx
+++ src/components/aboutModal.jsx
@@ -2,7 +2,7 @@ import { Modal, Frame, TitleBar, useModal } from "@react95/core";
 import { Explorer101, Mmsys113 } from "@react95/icons";
 import pixelme from "../shared/assets/pixelme.png";
 
-export default function AboutModal({ id = 'about'}) {
+export default function AboutModal({ id = 'about' }) {
     const { remove, minimize } = useModal();
 
     const handleClose = () => {
@@ -20,23 +20,20 @@ export default function AboutModal({ id = 'about'}) {
             }
         }} titleBarOptions={
             <>
-                <Modal.Minimize className="cursor-default"/>
+                <Modal.Minimize className="cursor-default" />
                 <TitleBar.Close onClick={handleClose} className="cursor-default" />
             </>
         } buttons={[{
-            value: 'Ok',
+            value: 'Próximo',
             onClick: handleButtonClick
-        }, {
-            value: 'Cancel',
-            onClick: handleButtonClick
-        }]}>
+        },]}>
             <Modal.Content width="400px" height="300px" boxShadow="$in" bgColor="white">
                 <Frame as="div" display="flex" flexDirection="column" gap="8px">
                     <h4 className="mt-1 text-center text-2xl">Vinicius Teixeira</h4>
                     <div className="flex items-start gap-4">
                         <img src={pixelme} height="250px" width="210px" alt="Vinicius Teixeira" className="mt-[-30px] ms-[-25px]" />
-                        <div className="pt-0">
-                            <p className="text-sm">Texto teste</p>
+                        <div>
+                            <p className="text-[0.90rem]">Sou um desenvolvedor fullstack apaixonado por transformar ideias em produtos digitais eficientes, unindo performance, criatividade e uma boa experiência ao usuário</p>
                         </div>
                     </div>
 
