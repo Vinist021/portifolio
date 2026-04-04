19ff880986845cb9e110fcfe4b3e1fb4b7ac7bab
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-04-04 14:14:38 -0300

    feat(TaskBar): adiciona ícone de experiência

--- src/components/desktopTaskBar.jsx
+++ src/components/desktopTaskBar.jsx
@@ -1,5 +1,6 @@
 import { Explorer101 } from "@react95/icons"
 import { TaskBar, List } from "@react95/core"
+import xp from "../shared/assets/xp.png"
 
 export default function DesktopTaskBar({ openAboutModal }) {
 
@@ -8,6 +9,9 @@ export default function DesktopTaskBar({ openAboutModal }) {
             <List width="225px">
                 <List.Item icon={<Explorer101 variant="32x32_4" />} onClick={() => openAboutModal()}>
                     Sobre
+                </List.Item>
+                <List.Item style={{ paddingInlineStart: 10 }} icon={<><img src={xp} alt="Experiência" width="32px" height="32px" /></>}>
+                    Experiência
                 </List.Item>
             </List>
         } />
