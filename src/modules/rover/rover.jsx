f629bcfd76c7667f81a69bac9aecfe981729db8b
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-06-06 22:21:26 -0300

    feat(rover): implementa bark sound

--- src/modules/rover/rover.jsx
+++ src/modules/rover/rover.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef } from "react";
 import { useClippy } from "@react95/clippy";
+import bark from "../../assets/audios/bark.mp3";
+
+const barkAudio = new Audio(bark);
 
 export default function Rover() {
   const { clippy } = useClippy();
@@ -9,6 +12,9 @@ export default function Rover() {
     if (!clippy || hasPlayedRef.current) return;
     hasPlayedRef.current = true;
     clippy.speak("Obrigado pela sua visita! Espero que goste da experiência.");
+    setTimeout(() => {
+      barkAudio.play();
+    }, 1900);
   }, [clippy]);
 
   return null;
