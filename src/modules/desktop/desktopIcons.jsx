b0dfff8d11fa94b48b4c2bf0d562ece4d3b76c6e
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-06-07 13:19:31 -0300

    feat(desktop): adiciona ícone CV

--- src/modules/desktop/desktopIcons.jsx
+++ src/modules/desktop/desktopIcons.jsx
@@ -1,9 +1,9 @@
 import { Cursor } from "@react95/core"
 import xp from "../../assets/images/xp.png"
 
-import { Explorer101, Progman11 } from "@react95/icons";
+import { Awschd32400, Explorer101, Progman11, Wordpad } from "@react95/icons";
 
-export default function DesktopIcons({ openAboutModal, openExperienceModal, openTechnologiesModal }) {
+export default function DesktopIcons({ openAboutModal, openExperienceModal, openTechnologiesModal, openCVModal }) {
     return (
         <>
             <div style={{ width: 100, marginLeft: 10, marginTop: 10 }}>
@@ -61,6 +61,26 @@ export default function DesktopIcons({ openAboutModal, openExperienceModal, open
                         }}
                     >
                         Tecnologias
+                    </p>
+                </div>
+
+                <div className={Cursor.Pointer} onClick={openCVModal}>
+                    <Awschd32400
+                        variant="32x32_4"
+                        style={{ marginLeft: 32, marginTop: 15 }}
+                    />
+                    <p
+                        style={{
+                            fontFamily: "MS Sans Serif",
+                            fontSize: 13,
+                            justifyContent: "center",
+                            marginTop: 5,
+                            width: 100,
+                            textAlign: "center",
+                            color: "black",
+                        }}
+                    >
+                        CV
                     </p>
                 </div>
             </div>
