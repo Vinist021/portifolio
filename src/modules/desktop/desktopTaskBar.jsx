bffef68ade04e9e6b14afee47fb87bfba99c90b6
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-06-07 23:06:32 -0300

    feat(taskBar): adiciona aba 'social'

--- src/modules/desktop/desktopTaskBar.jsx
+++ src/modules/desktop/desktopTaskBar.jsx
@@ -1,12 +1,36 @@
-import { Explorer101, Progman11, Awschd32400 } from "@react95/icons"
+import { Explorer101, Progman11, Awschd32400, Wab321019 } from "@react95/icons"
 import { TaskBar, List } from "@react95/core"
 import xp from "../../assets/images/xp.png"
+import { ListItem } from "@react95/core/ListItem"
+
+  const socials = [
+    {
+      id: 0,
+      name: "Github",
+      url: "https://github.com/Vinist021",
+    },
+    {
+      id: 1,
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/vinicius-dos-santos-teixeira/",
+    },
+  ];
 
 export default function DesktopTaskBar({ openAboutModal, openExperienceModal, openTechnologiesModal, openCVModal }) {
 
     return (
         <TaskBar list={
             <List width="225px">
+                <List.Item icon={<Wab321019 variant="32x32_4" />}>
+                    Social
+                    <List>
+                        {socials.map((social) => (
+                            <ListItem key={social.id} onClick={() => window.open(social.url, "_blank")}>
+                                {social.name}
+                            </ListItem>
+                        ))}
+                    </List>
+                </List.Item>
                 <List.Item icon={<Explorer101 variant="32x32_4" />} onClick={openAboutModal}>
                     Sobre
                 </List.Item>
