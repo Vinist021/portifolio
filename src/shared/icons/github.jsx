5b9be92915b5dea4e04291e613ea97f454c0ab83
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-06-10 23:01:28 -0300

    feat(CV): atualiza ícone Github

--- src/shared/icons/github.jsx
+++ src/shared/icons/github.jsx
@@ -8,7 +8,7 @@ export function Github(props) {
       shapeRendering="crispEdges"
       {...props}
     >
-        <path d="M4,0h4v2h-4z M14,0h4v2h-4z M4,2h2v2h-2z M8,2h6v2h-6z M16,2h2v2h-2z M2,4h2v6h-2z M18,4h2v6h-2z M4,10h2v2h-2z M16,10h2v2h-2z M0,12h2v2h-2z M6,12h2v2h-2z M14,12h2v2h-2z M2,14h4v2h-4z M8,14h2v2h-2z M12,14h2v2h-2z M6,16h2v4h-2z M14,16h2v4h-2z" />
+        <path d="M7,0h6v1h-6zM5,1h10v1h-10zM3,2h13v1h-13zM2,3h15v1h-15zM2,4h16v1h-16zM1,5h4v1h-4zM7,5h6v1h-6zM15,5h4v1h-4zM1,6h4v1h-4zM15,6h4v1h-4zM0,7h4v1h-4zM16,7h4v1h-4zM0,8h4v1h-4zM16,8h4v1h-4zM0,9h4v1h-4zM16,9h4v1h-4zM0,10h4v1h-4zM16,10h4v1h-4zM0,11h4v1h-4zM16,11h4v1h-4zM0,12h5v1h-5zM15,12h5v1h-5zM1,13h6v1h-6zM13,13h6v1h-6zM1,14h2v1h-2zM5,14h2v1h-2zM12,14h7v1h-7zM2,15h2v1h-2zM6,15h1v1h-1zM12,15h6v1h-6zM2,16h3v1h-3zM13,16h4v1h-4zM3,17h4v1h-4zM12,17h4v1h-4zM4,18h3v1h-3zM12,18h4v1h-4zM6,19h1v1h-1zM12,19h2v1h-2z" fill="black"/>
     </svg>
   );
 }
