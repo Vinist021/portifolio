f94711303fc899ad0ae100f1f9b5c864b73188a9
Vinist021 <viniciusds.teixeira03@gmail.com>
2026-08-25 23:03:00 -0300

    feat: ajusta modal sobre em dispositivos mobile

--- src/modules/modals/about/aboutModal.jsx
+++ src/modules/modals/about/aboutModal.jsx
@@ -30,9 +30,9 @@ export default function AboutModal({ id = 'about', nextStep }) {
             <Modal.Content className="responsive-modal__content" width="400px" height="300px" boxShadow="$in" bgColor="white">
                 <Frame as="div" display="flex" flexDirection="column" gap="8px" height="100%" minHeight="0">
                     <h4 className="mt-1 shrink-0 text-center text-xl md:text-2xl">Vinicius Teixeira</h4>
-                    <div className="about-content flex min-h-0 flex-1 flex-col items-center justify-center gap-2 md:flex-row md:items-start md:justify-start md:gap-4">
-                        <img src={pixelme} alt="Vinicius Teixeira" className="about-image h-auto w-[clamp(128px,42vw,170px)] max-w-full shrink-0 object-contain md:mt-[-30px] md:ms-[-25px] md:w-[210px]" />
-                        <div className="about-description min-w-0">
+                    <div className="about-content flex min-h-0 flex-1 flex-row items-start justify-start gap-2 md:gap-4">
+                        <img src={pixelme} alt="Vinicius Teixeira" className="about-image mt-[-18px] ms-[-14px] h-auto w-[clamp(112px,38vw,170px)] max-w-full shrink-0 object-contain md:mt-[-30px] md:ms-[-25px] md:w-[210px]" />
+                        <div className="about-description min-w-0 flex-1">
                             <p className="m-0 text-[0.90rem] leading-relaxed">Sou um desenvolvedor fullstack apaixonado por transformar ideias em produtos digitais eficientes, unindo performance, criatividade e uma boa experiência ao usuário</p>
                         </div>
                     </div>
